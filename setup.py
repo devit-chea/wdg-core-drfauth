0ec62c5e497cce9d06e064f77a543a04030d90d4
Devit Chea <devit.chea@wingbank.com.kh>
2025-04-19 12:18:39 +0700

    Update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="wdg-core-auth",
-    version="0.1.0",
+    version="0.2.0",
     author="Your Name",
     description="Reusable Django decorator for permission-based access control",
     long_description=long_description,
